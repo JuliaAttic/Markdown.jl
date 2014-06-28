9b11bee546eb272483d23f989b75e8d444d63e1b
Mike Innes <mike.j.innes@gmail.com>
2014-06-29 00:34:19 +0100

    script for the test suite

---
+++ markdown.jl
@@ -0,0 +1,35 @@
+#!/usr/local/bin/julia
+using Markdown
+
+input = readall(STDIN)
+
+cd(dirname(@__FILE__)) do
+  open("log.txt", "w") do io
+    println(io, input)
+    println(io, "----")
+  end
+end
+
+if input in ("\n", "")
+  println("no input")
+  exit(0)
+end
+
+try
+  output = stringmime("text/html", Markdown.parse(input))
+
+  println(output)
+
+  cd(dirname(@__FILE__)) do
+    open("log.txt", "a") do io
+      println(io, output)
+    end
+  end
+
+catch e
+  open("log.txt", "a") do io
+    showerror(io, e, catch_backtrace())
+    println(io)
+  end
+#   rethrow()
+end
