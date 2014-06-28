83aced1ee9785becc1cf71f807d2b0bd1e3a7042
Mike Innes <mike.j.innes@gmail.com>
2014-06-29 00:30:25 +0100

    remove lorem

--- src/Markdown.jl
+++ src/Markdown.jl
@@ -1,21 +1,5 @@
 module Markdown
 
-lorem = """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas
-ultrices eros non consectetur. Duis luctus tortor velit, sed varius sem
-tristique at. Sed consectetur luctus diam, nec eleifend sapien suscipit
-sit amet. Ut at dui at nibh varius egestas vel in mauris. Cras et nisl
-eu leo lacinia consectetur. Maecenas ornare massa magna, in faucibus
-tortor condimentum vel. Aliquam feugiat purus sit amet fringilla mollis.
-Proin a enim massa. Curabitur cursus velit quam, sit amet feugiat sem
-eleifend id. Ut velit leo, porta eget facilisis sed, suscipit vel felis.
-Aliquam at sem ac metus tempor tincidunt et non mi. Proin ipsum nunc,
-molestie vel urna eget, gravida aliquam neque. Praesent elementum, quam
-et rutrum fringilla, mi tortor gravida nisl, non tempor mi urna sed
-urna. Cras vel viverra dui, id eleifend nunc. Ut diam tortor, sagittis
-ut venenatis in, fermentum id risus.
-"""
-
 include("types.jl")
 include("parse/parse.jl")
 include("parse/standard.jl")
