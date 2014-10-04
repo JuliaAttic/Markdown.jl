67597e8fecf29193028d2949f36b942b8fe9857c
Mike Innes <mike.j.innes@gmail.com>
2014-10-04 12:09:52 +0100

    add interpolation to Julia flavour

--- src/Julia/Julia.jl
+++ src/Julia/Julia.jl
@@ -5,5 +5,7 @@ Julia easy peasy.
 We start by borrowing GitHub's `fencedcode` extension – more to follow.
 """
 
-@flavour julia [hashheader, list, indentcode, fencedcode, blockquote, paragraph,
-                escapes, en_dash, inline_code, asterisk_bold, asterisk_italic, image, link]
+include("interp.jl")
+
+@flavour julia [blockinterp, hashheader, list, indentcode, fencedcode, blockquote, paragraph,
+                escapes, interp, en_dash, inline_code, asterisk_bold, asterisk_italic, image, link]
